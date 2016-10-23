4a998c1756a974e5251d7a51940d4c3baa63633c
iharsimran30 <iharsimran30@gmail.com>
2016-10-22 23:29:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$('button').click(function() {
+    $('.button-container').append('<h1>You have Clicked The Button</h1>');
+});
+
+// JSX is HTML inside the JavaScript
+
+render : function () {
+    return (
+        <div id="divOne">Hello World</div>
+    );
+}
